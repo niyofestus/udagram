71f9b41b945ace01806ca65ebb29c123499c3702
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2021-04-04 08:57:17 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,103 +1,95 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
-import * as EmailValidator from 'email-validator';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
-
-const TEST_EMAIL = "test@user.com";
-const TEST_PASS = "ThisIsMyPassw0rd";
+import { reject } from 'bluebird';
 
 (async () => {
 
-    const app = express();
+  // Init the Express application
+  const app = express();
 
-    const port = process.env.PORT || 8082; // env port or default port to listen
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // regex to verify image_url format
+  const image_url_regex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpeg|jpg|gif|png|svg)/
 
-    app.use(bodyParser.json());
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
 
-    // to check of url contain an image file type?
-    const isImageUrl = require('is-image-url');
+  // middleware checking url and returing message to try filteredimage url
+  app.use(function (req, res, next) {
+    if (req.path != '/filteredimage') {
+      res.send("try GET /filteredimage?image_url={{}}")
+    }
+    next()
+  })
 
-    // Verify URL if it exist?
-    const urlExists = require('url-exists-deep');
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+  /**************************************************************************** */
 
-    //VERY BAD
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
+  app.get("/filteredimage", async (req, res) => {
+    //
+    let { image_url } = req.query;
+    
+    // return error if parameter is missing
+    if (!image_url) {
+      res.status(400).send("image url missing")
+    }else {
 
-    // Root URI call
-    app.get("/", async (req, res) => {
-        res.send("Try /filteredimage?image_url=\"\" with actual image url.")
-    });
-
-
-    // Filter an image based on a public url
-    app.get("/filteredimage", async (req: Request, res: Response) => {
-        const email = req.body.email;
-        const password = req.body.password;
-
-        // This is not secure way to handle email and password but I had to save constants as some of my tests were
-        // not picking system env variables
-
-        // Verifying email and password, validating email address
-        if (!email || !password || !EmailValidator.validate(email)) {
-            return res.status(400).send({auth: false, message: `Failed to authenticate request.`});
+      // return error if image url format is incorrect
+      console.log(image_url.match(image_url_regex))
+      if (!image_url.match(image_url_regex)) {
+        res.status(400).send("image url format is incorrect, example url : https://some_domain(like google.com)/probably_some_more_paths/image_name.png/jpg/gif")
+      }else {
+        
+        try {
+          let image_response = await filterImageFromURL(image_url)
+          // if image reader does not fail return image else return error
+          if(image_response != "no image found"){
+            res.status(200).sendFile(image_response, async callback=>{
+              await deleteLocalFiles([image_response])
+            })
+          } else {
+            res.status(200).send("no image found in the given url")
+          }
+        } catch (err) {
+          // other errors
+          console.error(err)
+          res.status(200).send("image processing failed")
         }
+      }
+    }
 
-        if (email !== (process.env.SERVER_USER || TEST_EMAIL)) {
-            return res.status(400).send({auth: false, message: `Failed to authenticate request.`});
-        }
-        if (password !== (process.env.SERVER_PASSWORD|| TEST_PASS)) {
-            return res.status(400).send({auth: false, message: `Failed to authenticate request.`});
-        }
-
-        // publicly accessible image URL
-        const {image_url} = req.query;
-
-        // Verifying image_url query and validating url
-        if (!image_url || !isImageUrl(image_url)) {
-            return res.status(400).send('`image_url` is required!');
-        }
-
-        // Validating image_url
-        urlExists(image_url).then(function (exists: { href: any; }) {
-            if (!exists) {
-                return res.status(400).send('The `image_url` is bad!');
-            } else {
-
-                // Filter the image using helper function
-                filterImageFromURL(image_url).then((data) => {
-
-                    // Send the filtered image file in the response
-                    return res.sendFile(data, {}, function (err) {
-                        if (err) {
-                            return res.status(500).send('Something went wrong!');
-                        } else {
-
-                            // Deleting file using helper function
-                            deleteLocalFiles([data]).catch((err) => {
-                                if (err) {
-                                    return res.status(400).send('The `image_url` is bad!');
-                                }
-                            });
-                        }
-                    });
-                }).catch((err) => {
-                    if (err) {
-                        return res.status(500).send('Something went wrong!');
-                    }
-                })
-            }
-        });
-    });
+    //
+  } );
 
 
-    // Start the Server
-    app.listen(port, () => {
-        console.log(`server running http://localhost:${ port }`);
-        console.log(`press CTRL+C to stop server`);
-    });
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
 })();
